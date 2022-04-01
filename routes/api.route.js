fb7ba20c5aede90dfd9da6e89cf914efe5c523ec
Argie Barcena <barcenaargie@gmail.com>
2022-04-02 03:28:44 +0800

    creating tokens base on reponse of google calendar oAuth2

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
+const { google } = require('googleapis')
+
+const REACT_APP_GOOGLE_OAUTH_CLIENT_ID = process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID
+const REACT_APP_GOOGLE_OAUTH_CLIENT_SECRET = process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_SECRET
+
+const oauth2Client = new google.auth.OAuth2(
+  REACT_APP_GOOGLE_OAUTH_CLIENT_ID,
+  REACT_APP_GOOGLE_OAUTH_CLIENT_SECRET,
+  process.env.REACT_APP_LOCAL_BASE_URL
+)
 
 router.get('/', async (req, res, next) => {
   res.send({ message: 'Ok api is working 🚀' });
 });
+
+router.post('/create-tokens', async (req, res, next) => {
+  try {
+    const { code } = req.body 
+    const {tokens} = await oauth2Client.getToken(code)
+    res.send(tokens)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// router.post('/create-event', async (req, res, next) => {
+//   try {
+
+//   } catch (error) {
+//     next(error)
+//   }
+// })
 
 module.exports = router;
